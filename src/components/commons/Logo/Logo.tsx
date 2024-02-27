ba8d9dd4dc55381ea2f93be125c0f1eed1110cdf
Mika <mikberza@mail.ru>
2024-02-27 09:18:22 +0300

    Add (my-app): route added #1 #1 -added a route to the component (Logo) to return to the main page (HomePage)
    Добавить (Logo): добавлен маршрут #1
    #1
    -добавлен маршрут компоненту (Logo) для возврата на главную страницу (HomePage)

--- src/components/commons/Logo/Logo.tsx
+++ src/components/commons/Logo/Logo.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import images from '../../../assets/images/fav_icons/logo192.png';
 import styles from './Logo.module.css';
 
 const Logo = () => {
-  return <img className={styles.images} src={images} alt='images' />;
+  return (
+    <Link to='/layout_Test_Tasks_R_RT_T'>
+      <img className={styles.images} src={images} alt='images' />
+    </Link>
+  );
 };
 
 Logo.displayName = 'Logo';
